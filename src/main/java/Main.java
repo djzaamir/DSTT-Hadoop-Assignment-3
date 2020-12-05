1432e40896b3c5d0e278ade6737e8adb5f97ee24
Aamir <djzaamir@gmail.com>
2020-12-05 13:44:07 +0500

    Added Some Helpful resources in the important-notes... files, as well as completed basic setup for basic java project

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/*
+* Main Driver Class
+* Responsible for creating hadoop jobs, submitting them to hadoop cluster and collecting results
+* */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey now brown cow");
+    }
+}
